1d45de72784b627e74550e646400ceba4cf3a74c
stateandprove <sphere050@yandex.ru>
2022-05-19 15:56:41 +0200

    init

---
+++ app/app.py
@@ -0,0 +1,26 @@
+from jira import JIRA
+from flask import Flask, request, make_response
+
+import environ
+
+import json
+
+
+app = Flask(__name__)
+
+env = environ.Env(
+    DEBUG=(bool, False),
+)
+
+
+@app.route('/jira-helper/jira-event', methods=['POST'])
+def print_response():
+    if request.method == 'POST':
+        data = request.data
+        json_data = json.loads(data)
+        print(json.dumps(json_data, indent=4))
+        return "OK"
+
+
+if __name__ == '__main__':
+    app.run(debug=env("DEBUG"), host='0.0.0.0', port=8080)
